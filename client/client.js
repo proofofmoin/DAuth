1ee74f6b3142efbd97aa7433e678965ec472a65f
Mauricio Cifuentes <mcifuent@gmail.com>
2019-09-07 12:11:31 +0200

    pushing client part raw

--- client/client.js
+++ client/client.js
@@ -1,10 +1,42 @@
+// require blockchain-core dependencies
 const Web3 = require('web3');
 
-var Accounts = require('web3-eth-accounts');
+// require blockchain-core
+const { Ipfs, createDefaultRuntime, DigitalTwin, Container } = require('@evan.network/api-blockchain-core');
 
-// Passing in the eth or web3 package is necessary to allow retrieving chainId, gasPrice and nonce automatically
-// for accounts.signTransaction().
-var accounts = new Accounts('ws://localhost:8546');
+// ipfs configuration for evan.network testnet storage
+const ipfsConfig = { host: 'ipfs.test.evan.network', port: '443', protocol: 'https' };
 
-console.log(accounts);
+// web3 provider config (currently evan.network testcore)
+const web3Provider = 'wss://testcore.evan.network/ws'
+
+async function init() {
+    // initialize dependencies
+    const provider = new Web3.providers.WebsocketProvider(
+        web3Provider,
+        { clientConfig: { keepalive: true, keepaliveInterval: 5000 } });
+    const web3 = new Web3(provider, { transactionConfirmationBlocks: 1 });
+    const dfs = new Ipfs({ dfsConfig: ipfsConfig });
+
+    // create runtime
+    const runtime = await createDefaultRuntime(
+        web3,
+        dfs,
+        {
+            // mnemonic: 'connect neither prefer select wild grit shield vast tornado blouse record flat',
+            // password: 'Password123'
+            mnemonic: 'omit champion track input wet match enemy uncover slim summer assume pill',
+            password: 'Latercera19'
+        }
+    );
+
+    const privateKey = await runtime.executor.signer.accountStore.getPrivateKey(runtime.activeAccount);
+    console.log("prive", privateKey)
+
+    const siginfo = runtime.web3.eth.accounts.sign("AltTubeRocks", `0x${privateKey}`)
+    console.log("signature == ", siginfo);
+}
+
+
+init();
 

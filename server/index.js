f4d0683f4a32fac444038df2eacaa1bbfe0d9785
suryanarayanamurthy <suryanarayan.murthy@gmail.com>
2019-09-07 13:38:34 -0500

    point to google.de instead of .com

--- server/index.js
+++ server/index.js
@@ -47,7 +47,7 @@ app.get('/shakehand',function(req, res) {
   //console.log(productionProfile)
   if (productionProfile.authorizedusers.includes(unsign)){
    
-request('http://www.google.com', function (error, response, body) {
+request('https://www.google.de', function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   res.send(body) // Print the HTML for the Google homepage.
